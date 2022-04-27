2dec89a37461593677860a693e72813ef9adda81
Martin Picek <martinpicek4@gmail.com>
2022-04-27 20:56:02 +0200

    Add RotEq + FullGaussian2d network

--- models.py
+++ models.py
@@ -376,8 +376,8 @@ class LurzRotEq(encoding_model_fixed):
             in_shape=( #TODO: stack???
                 #TODO: ten shape si potvrdit
                 self.config["core_hidden_channels"] * self.config["num_rotations"] * abs(self.config["stack"]),
-                self.config["input_size_x"] * self.config["upsampling"], # ocividne se to padduje, takze to neztraci rozmery
-                self.config["input_size_y"] * self.config["upsampling"],
+                self.config["input_size_x"], # ocividne se to padduje, takze to neztraci rozmery
+                self.config["input_size_y"],
             ),
             outdims=self.config["num_neurons"],
             bias=self.config["readout_bias"],
@@ -456,6 +456,90 @@ class LurzRotEq(encoding_model_fixed):
         self.log("reg/core reg", core_reg)
         self.log("reg/readout_reg", readout_reg)
         return reg_term
+
+
+class LurzRotEqGauss(encoding_model_fixed):
+    """Lurz's model with RotEq core"""
+
+    def __init__(self, **config):
+        super().__init__(**config)
+        self.config = config
+        # self.loss = PoissonLoss(avg=True)
+        # self.corr = Corr()
+        self.nonlinearity = self.config["nonlinearity"]
+
+        self.core = RotationEquivariant2dCore(
+            num_rotations=self.config["num_rotations"],
+            stride=self.config["stride"],
+            upsampling=self.config["upsampling"],
+            rot_eq_batch_norm=self.config["rot_eq_batch_norm"],
+            input_regularizer=self.config["input_regularizer"],
+            input_channels=self.config["input_channels"],
+            hidden_channels=self.config["core_hidden_channels"],
+            input_kern=self.config["core_input_kern"],
+            hidden_kern=self.config["core_hidden_kern"],
+            layers=self.config["core_layers"],
+            gamma_input=config["core_gamma_input"],
+            gamma_hidden=config["core_gamma_hidden"],
+            stack=config["stack"],
+            depth_separable=config["depth_separable"],
+            use_avg_reg=config["use_avg_reg"]
+        )
+
+
+        self.readout = readouts.FullGaussian2d(
+            in_shape=( #TODO: stack???
+                #TODO: ten shape si potvrdit
+                self.config["core_hidden_channels"] * self.config["num_rotations"] * abs(self.config["stack"]),
+                self.config["input_size_x"], # ocividne se to padduje, takze to neztraci rozmery
+                self.config["input_size_y"],
+            ),
+            outdims=self.config["num_neurons"],
+            bias=self.config["readout_bias"],
+            mean_activity=self.config["mean_activity"],
+            feature_reg_weight=self.config["readout_gamma"],
+        )
+
+        self.register_buffer("laplace", torch.from_numpy(laplace()))
+        self.nonlin = bl.act_func()[config["nonlinearity"]]
+
+    def forward(self, x):
+        x = self.core(x)
+        x = self.nonlin(self.readout(x))
+        return x
+    
+    def __str__(self):
+        return "RotEq_FullGaussian2d"
+    
+
+    def reg_readout_group_sparsity(self):
+        nw = self.readout.features.reshape(self.config["num_neurons"], -1)
+        reg_term = self.config["reg_group_sparsity"] * torch.sum(
+            torch.sqrt(torch.sum(torch.pow(nw, 2), dim=-1)), 0
+        )
+        return reg_term
+
+    def regularization(self):
+
+        readout_l1_reg = self.readout.regularizer(reduction="mean")
+        self.log("reg/readout_l1_reg", readout_l1_reg)
+        # group_sparsity = self.reg_readout_group_sparsity()
+        # self.log("reg/readout_group_sparsity", group_sparsity)
+
+        readout_reg = readout_l1_reg
+        # for k in self.config.keys():
+        #     # print(k)
+        #     if k.startswith("reg_readout"):
+        #         kreg = getattr(self, k)()
+        #         readout_reg = readout_reg + kreg
+        # core
+        core_reg = self.core.regularizer()
+        reg_term = readout_reg + core_reg
+        self.log("reg/core reg", core_reg)
+        self.log("reg/readout_reg", readout_reg)
+        return reg_term
+
+    
 
 class LurzGauss(encoding_model_fixed):
     """Lurz's model"""
