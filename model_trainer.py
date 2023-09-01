9afd9d2802dff67db968e181f8d13f00dfc6181d
Martin Picek <martinpicek4@gmail.com>
2023-09-01 08:18:21 +0200

    Clean model trainer

--- model_trainer.py
+++ model_trainer.py
@@ -14,78 +14,7 @@ from pprint import pprint
 from Antolik_dataset import AntolikDataModule
 
 
-def get_best_model(
-    wandb_run,
-    model_class=reCNN_bottleneck_CyclicGauss3d,
-    model_artifact_name="reCNN_bottleneck_CyclicGauss3d",
-):
-    """Returns the best model
-
-    Args:
-        wandb_run (wandb.run): Wandb run for logging
-        model_class (class, optional): The class of the model. Defaults to reCNN_bottleneck_CyclicGauss3d.
-        model_artifact_name (str, optional): Name of the artifact of the model. Defaults to "reCNN_bottleneck_CyclicGauss3d".
-
-    Returns:
-        pl.model: The best model
-    """
-
-    if model_artifact_name == None:
-        model_artifact_name = model_class.__str__()
-
-    model_artifact = wandb_run.use_artifact(model_artifact_name + ":best")
-    model_checkpoint_path = model_artifact.download()
-
-    best_model = model_class.load_from_checkpoint(model_checkpoint_path)
-
-    return best_model
-
-
-def Lurz_dataset_preparation_function(config, run=None):
-    """Gets config, can edit it.
-        Returns Pytorch Lightning DataModule of the dataset from Lurz's publication
-
-    Args:
-        config (dict): Configuration dictionary
-        run (wandb.run, optional): Wandb run for logging. If None, nothing is logged. Defaults to None.
-
-    Returns:
-        pl.DataModule: Pytorch Lightning DataModule of the dataset.
-    """    
-
-    # setup datamodule - use artifact
-    data_dir = "data/lurz2020/static20457-5-9-preproc0"
-
-    if run is not None:
-        dataset_artifact = run.use_artifact(config["dataset_artifact_name"])
-        data_dir = dataset_artifact.download()
-
-    dataset_config = {
-        "data_dir": data_dir,
-        "batch_size": config["batch_size"],
-        "normalize": True,
-        "exclude": "images",
-    }
-
-    dm = LurzDataModule(**dataset_config)
-    dm.prepare_data()
-    dm.setup()
-
-    # update config for initialization of model (<- certain config parameters depend on data)
-    config.update(
-        {
-            "input_channels": dm.get_input_shape()[0],
-            "input_size_x": dm.get_input_shape()[1],
-            "input_size_y": dm.get_input_shape()[2],
-            "num_neurons": dm.get_output_shape()[0],
-            "mean_activity": dm.get_mean(),
-        }
-    )
-
-    return dm
-
-
-def Antolik_dataset_preparation_function(config, run=None, load_data=True):
+def Antolik_dataset_preparation_function(config, load_data=True):
     """Gets config, can edit it.
         Uses only Antolik's in-silico dataset
 
@@ -124,8 +53,6 @@ def Antolik_dataset_preparation_function(config, run=None, load_data=True):
             "num_workers": 0,
         }
 
-    if run is not None:
-        raise NotImplementedError()
 
     dm = AntolikDataModule(**dataset_config)
 
@@ -221,7 +148,6 @@ def run_wandb_training(
         _type_: _description_
     """
 
-
     # init wandb run
     run = wandb.init(
         config=config,
@@ -235,10 +161,9 @@ def run_wandb_training(
     pl.seed_everything(config["seed"], workers=True)
     
     pprint(config)
-
     print("Setting up the dataset...")
 
-    dm = dataset_preparation_function(config, None)
+    dm = dataset_preparation_function(config)
 
     model = get_model(config, dm, model_class)
     # summary(model, torch.zeros((config["batch_size"], dm.get_input_shape()[0], dm.get_input_shape()[1], dm.get_input_shape()[2])))
@@ -347,15 +272,9 @@ def run_wandb_training(
 def run_training_without_logging(
     config,
     dataset_preparation_function,
-    entity,
-    project,
-    model_artifact_name=None,
     model_class=reCNN_FullFactorized,
     early_stopping_monitor="val/corr",
     early_stopping_mode="max",
-    model_checkpoint_monitor="val/corr",
-    model_checkpoint_mode="max",
-    # **config,
 ):
     """Sets up a dataset and a model and runs a model
         training WITHOUT Wandb. Subsequently it evaluates the model
@@ -379,28 +298,12 @@ def run_training_without_logging(
 
     pl.seed_everything(config["seed"], workers=True)
 
-    # # init wandb run
-    # run = wandb.init(
-    #     config=config,
-    #     project=project,
-    #     entity=entity,
-    # )
-
-    # Access all hyperparameter values through wandb.config
-    # config = dict(wandb.config)
     pprint(config)
-    pl.seed_everything(config["seed"], workers=True)
 
-    dm = dataset_preparation_function(config, None)
+    dm = dataset_preparation_function(config)
 
     # Set up model
     model = get_model(config, dm, model_class)
-
-    # summary(model, torch.zeros((config["batch_size"], dm.get_input_shape()[0], dm.get_input_shape()[1], dm.get_input_shape()[2])))
-
-    # setup wandb logger
-    # wandb_logger = WandbLogger(log_model=True)
-    # wandb_logger.watch(model, log="all", log_freq=250)
 
     # define callbacks for the training
     early_stop = EarlyStopping(
@@ -408,13 +311,6 @@ def run_training_without_logging(
         patience=config["patience"],
         mode=early_stopping_mode,
     )
-    # checkpoint_callback = ModelCheckpoint(
-    #     save_top_k=1, monitor=model_checkpoint_monitor, mode=model_checkpoint_mode
-    # )
-
-    # checkpoint_callback = ModelCheckpoint(
-    #     dirpath="/storage/brno2/home/mpicek/MODEL_CHECKPOINTS/" + wandb.run.name, save_top_k=1, monitor=model_checkpoint_monitor, mode=model_checkpoint_mode
-    # )
 
     # define the trainer
     trainer = pl.Trainer(
@@ -423,7 +319,6 @@ def run_training_without_logging(
         gpus=[0],
         logger=False,
         log_every_n_steps=100,
-        # deterministic=True,
         enable_checkpointing=True,
     )
 
@@ -440,29 +335,5 @@ def run_training_without_logging(
             train_dataloaders=dm.train_dataloader(),
             val_dataloaders=dm.val_dataloader(),
         )
-
-    # best_observed_val_metric = (
-    #     checkpoint_callback.best_model_score.cpu().detach().numpy()
-    # )
-    # print(
-    #     "Best model's "
-    #     + config["observed_val_metric"]
-    #     + ": "
-    #     + str(best_observed_val_metric)
-    # )
-
-    # add best corr to metadata
-    # metadata = {**config, "best_model_score": best_observed_val_metric}
-
-    # print(checkpoint_callback.best_model_path)
-
-    # model = model_class.load_from_checkpoint(checkpoint_callback.best_model_path)
-
-    # if config["test"]:
-    #     dm.model_performances(model, trainer)
-
-        # result_artifact = wandb.Artifact(name="RESULT_" + model_artifact_name, type="result",
-        #     metadata=results[0])
-        # run.log_artifact(result_artifact)
 
     return model
